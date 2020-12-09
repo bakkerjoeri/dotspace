c56a7d873dc6f156195d44addbb64495a9cb985a
Joeri Bakker <bakkerjoeri@gmail.com>
2020-12-09 22:38:04 +0100

    1.0.0

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+export declare type Vector = number[];
+export declare type Vector2D = [number, number];
+export declare type Vector3D = [number, number, number];
+export declare function equals(...vectors: Array<Vector>): boolean;
+export declare function add(...vectors: Array<Vector>): Vector;
+export declare function multiplyByScalar(scalar: number, vector: Vector): Vector;
+export declare function dotProduct(vectorA: Vector, vectorB: Vector): number;
+export declare function crossProduct(a: Vector, b: Vector): Vector;
+export declare function magnitude(vector: Vector): number;
---
+++ dist/index.js
@@ -0,0 +1,62 @@
+export function equals(...vectors) {
+    if (vectors.length === 0) {
+        throw new Error('Can\'t check equality for 0 vectors.');
+    }
+    return vectors.every((currentVector, index) => {
+        if (index === 0) {
+            return true;
+        }
+        const previousVector = vectors[index - 1];
+        if (currentVector.length !== previousVector.length) {
+            return false;
+        }
+        return currentVector.every((component, index) => {
+            return component === previousVector[index];
+        });
+    }, true);
+}
+export function add(...vectors) {
+    if (vectors.length === 0) {
+        throw new Error('Can\'t add 0 vectors.');
+    }
+    return vectors.reduce((totalVector, currentVector) => {
+        if (!totalVector) {
+            return currentVector;
+        }
+        if (currentVector.length !== totalVector.length) {
+            throw new Error(`Can't add vectors with differing lengths. Expected a length of ${totalVector.length}, but got a length of ${currentVector.length}.`);
+        }
+        return totalVector.map((component, index) => {
+            return component + currentVector[index];
+        });
+    });
+}
+export function multiplyByScalar(scalar, vector) {
+    return vector.map(component => {
+        return component * scalar;
+    });
+}
+export function dotProduct(vectorA, vectorB) {
+    if (vectorA.length !== vectorB.length) {
+        throw new Error(`Can't get the dot product of vectors with differing lengths. Expected a length of ${vectorA.length}, but got a length of ${vectorB.length}.`);
+    }
+    return vectorA.reduce((total, componentA, index) => {
+        const componentB = vectorB[index];
+        return total + componentA * componentB;
+    }, 0);
+}
+export function crossProduct(a, b) {
+    if (a.length !== 3 || b.length !== 3) {
+        throw new Error(`Expected a pair of 3-dimensional vectors, but got a vector with ${a.length} dimensions and another with ${b.length}.`);
+    }
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0],
+    ];
+}
+export function magnitude(vector) {
+    return Math.sqrt(vector.reduce((result, currentComponent) => {
+        return result + Math.pow(currentComponent, 2);
+    }, 0));
+}
